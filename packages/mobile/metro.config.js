b6be2893ffb031c2626d01e5902a4ad9488ce708
jsu <41497174+jsubloom@users.noreply.github.com>
2023-09-01 02:22:09 -0500

    [mobile] Add more config for external  paths

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -24,5 +24,6 @@ if (!config.watchFolders) {
 // This is necessary for local packages (yarn add link, yarn link) or EXTERNAL tsconfig.json path aliases to work
 // Regarding local packages, this is partially inspired by https://medium.com/@alielmajdaoui/linking-local-packages-in-react-native-the-right-way-2ac6587dcfa2
 config.watchFolders.push(path.resolve(__dirname + "/../shared/dist"));
+config.watchFolders.push(path.resolve(__dirname + "/../web"));
 
 module.exports = config;
